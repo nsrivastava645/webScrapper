29caf6338cf1808d3860f7aac05f35023bbcac87
nsrivastava645 <neeraj@ballebaazi.com>
2022-04-28 02:20:35 +0530

    error handling done

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import Express from "express";
 import reviewsCrawlerRoute from "./src/routes/reviewsCrawlerRoute.js";
-
+import chalk from "chalk";
+import dotenv from "dotenv";
+dotenv.config();
+const PORT = process.env.PORT || 2020;
 const app = Express();
 app.use(Express.json());
-app.use(Express.urlencoded({extended: false}));
-app.use('/reviews', reviewsCrawlerRoute);
-app.listen(2020, () => {
-  console.log("App running on port 2020");
+app.use(Express.urlencoded({ extended: false }));
+app.use("/reviews", reviewsCrawlerRoute);
+app.listen(PORT, () => {
+  console.log(chalk.bgBlueBright.whiteBright(`server running on port ${PORT}`));
 });
--- src/routes/reviewsCrawlerRoute.js
+++ src/routes/reviewsCrawlerRoute.js
@@ -1,4 +1,6 @@
-import {Router} from "express";
-import ReviewsCrawlerController from "../controllers/reviewsCrawlerController.js"
-export default Router()
-    .post('/getReviews', ReviewsCrawlerController.getReviews)
+import { Router } from "express";
+import ReviewsCrawlerController from "../controllers/reviewsCrawlerController.js";
+export default Router().post(
+  "/getReviews",
+  ReviewsCrawlerController.getReviews
+);
